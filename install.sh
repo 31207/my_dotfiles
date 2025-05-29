9bc3fdaf64d3b6c44383d1e2b06b8d754344023d
31207 <1692038362@qq.com>
2025-05-29 20:47:48 +0800

    1

--- install.sh
+++ install.sh
@@ -9,20 +9,22 @@ echo "正在将配置文件复制到 $CONFIG_DIR ..."
 
 # 遍历每个目录
 for dir in "${DIRS[@]}"; do
-    SRC="$DOTFILES_DIR/$dir"
-    DEST="$CONFIG_DIR/$dir"
-    sudo rm -rf "$DEST"
-    # 创建目标目录
-    sudo mkdir -p "$DEST"
+	SRC="$DOTFILES_DIR/$dir"
+	DEST="$CONFIG_DIR/$dir"
+	sudo rm -rf "$DEST"
+	# 创建目标目录
+	sudo mkdir -p "$DEST"
 
-    # 复制内容
-    sudo cp -r "$SRC/"* "$DEST/"
-    echo "已复制 $dir 到 $DEST"
+	# 复制内容
+	sudo cp -r "$SRC/"* "$DEST/"
+	echo "已复制 $dir 到 $DEST"
 done
 
 # 单独处理 picom.conf
 cp "$DOTFILES_DIR/picom.conf" "$CONFIG_DIR/picom.conf"
 echo "已复制 picom.conf 到 $CONFIG_DIR"
+cp "$DOTFILES_DIR/feather.ttf" "$HOME/.local/share/fonts/feather.ttf"
+echo "Font has been installed."
 sudo chmod +x ~/.config/i3/*.sh
 
 echo "配置文件复制完成。"
