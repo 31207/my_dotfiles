5026b45c1b7a99124e15852701adc741a20334be
31207 <1692038362@qq.com>
2025-11-13 19:11:49 +0800

    delete folder go-musicfox

--- home.nix
+++ home.nix
@@ -27,12 +27,6 @@
     executable = true;  # 将其中所有文件添加「执行」权限
   };
 
-  home.file.".config/go-musicfox" = {
-    source = ./my_dotfiles/go-musicfox;
-    recursive = true;   # 递归整个文件夹
-    executable = true;  # 将其中所有文件添加「执行」权限
-  };
-
   home.file.".config/i3" = {
     source = ./my_dotfiles/i3;
     recursive = true;   # 递归整个文件夹
